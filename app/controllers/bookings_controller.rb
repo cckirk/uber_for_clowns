a07aacd129877f941ff59410c7662e1b9fa76642
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-01 20:50:59 -0400

    adding booking controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,17 @@
+class BookingsController < ApplicationController
+  def create
+    booking = Booking.new(
+      price: params[:price],
+      length: params[:length],
+      user_id: params[:user_id],
+      clown_id: params[:clown_id],
+      date: params[:date]
+    )
+    booking.save
+    if booking.save
+      render json: { message: "Booking created successfully" }, status: :created
+    else
+      render json: { errors: Booking.errors.full_messages }, status: :bad_request
+    end
+  end
+end
